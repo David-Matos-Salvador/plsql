dc25a334cfdd0f6470f20841394a5606a8c100f1
David-Matos-Salvador <61066992+David-Matos-Salvador@users.noreply.github.com>
2020-11-12 20:35:50 -0500

    nueva actualizacaion

--- como_puedo_restar_una_previa_fila.sql
+++ como_puedo_restar_una_previa_fila.sql
@@ -15,4 +15,15 @@ from t;
   --La funcion LAG es una funcion Analitica que permite consultar mas que una fila en una tabla a la vez sin tener que unir con otra tabla a sí misma.
   --Devuelve valores de una fila previa de la tabla.
   
+--In earlier versions of SQL Server, you can do almost the same thing using a correlated subquery:
+--En anteriores versiones de SQL SERVER,tu puedes hacer lo mismo usando un subconsulta correlacionada
+
+select col,
+          col - isnull((select top 1 col
+                        from t t2
+                        where t2.id < t.id
+                        order by id desc
+                       ), 0)
+from t
+  
   
