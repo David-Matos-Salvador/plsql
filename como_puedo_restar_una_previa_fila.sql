7c637e9512ed19e8c3070442399ac22158a8f337
David-Matos-Salvador <61066992+David-Matos-Salvador@users.noreply.github.com>
2020-11-12 19:15:32 -0500

    como restar una file previa
    vamos por mas  ... :)

---
+++ como_puedo_restar_una_previa_fila.sql
@@ -0,0 +1,18 @@
+--ASUMIENDO QUE TENGO ORDENADO LA FILAS 
+
+select col,
+          col - coalesce(lag(col) over (order by id), 0) as diff
+from t;
+
+---COALESCE 
+  --COALESCE returns the first non-null expr in the expression list. You must specify at least two expressions.
+  --If all occurrences of expr evaluate to null, then the function returns null.
+  --COALESCE retorna el primer no nullo expresion en la lista.Deberias especificar al menos 2 expresiones.
+  --If todas la expresiones retornan nullo,entonces la funcion retornara nullo.
+---LAG 
+  --The Oracle/PLSQL LAG function is an analytic function that lets you query more than one row in a table at a time without having to join the table to itself.
+  --It returns values from a previous row in the table.
+  --La funcion LAG es una funcion Analitica que permite consultar mas que una fila en una tabla a la vez sin tener que unir con otra tabla a sí misma.
+  --Devuelve valores de una fila previa de la tabla.
+  
+  
